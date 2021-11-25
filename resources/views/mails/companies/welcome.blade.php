8780b98842afde4021ea748cab8937d13ccbe653
vianafgbluiz <vianafgbluiz@ufu.br>
2021-11-24 21:18:05 -0300

    finish course

---
+++ resources/views/mails/companies/welcome.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+    # Olá!
+
+    Seja muito bem vindo(a) ao nosso sistema
+
+    Obrigado,
+    {{ config('app.name') }}
+@endcomponent
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,19 @@
 <?php
 
+
+use App\Jobs\CompanyCreatedJob;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
+    return response()->json([
+        'success' => true,
+        'message' => 'Sucesso ao retornar'
+    ]);
+});
+
+Route::get('/teste', function () {
+    CompanyCreatedJob::dispatch('teste@gmail.com')->onQueue('queue_email');
     return response()->json([
         'success' => true,
         'message' => 'Sucesso ao retornar'
